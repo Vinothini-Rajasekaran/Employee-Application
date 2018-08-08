6fc875577b264abe8f7ec0ac2455bdad6b497e15
Lavanya Saravanan <lavanya.saravanan-1@ou.edu>
2018-08-08 14:30:01 -0500

    Sign_up validation

--- Lav_Vino_Project/Signup.aspx.designer.cs
+++ Lav_Vino_Project/Signup.aspx.designer.cs
@@ -40,12 +40,12 @@ namespace Lav_Vino_Project {
         protected global::System.Web.UI.WebControls.TextBox eid;
         
         /// <summary>
-        /// login control.
+        /// signup control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.Button login;
+        protected global::System.Web.UI.WebControls.Button signup;
     }
 }
