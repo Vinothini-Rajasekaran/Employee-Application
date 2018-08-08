cd0681f6b2b72039ae3a47004ec651ce82ff40d8
Vinothini-Rajasekaran <vinothini.rajasekaran-1@ou.edu>
2018-08-08 16:07:54 -0500

    signup validation

--- Lav_Vino_Project/Signup.aspx.cs
+++ Lav_Vino_Project/Signup.aspx.cs
@@ -1,5 +1,7 @@
 ﻿using System;
-
+using System.Collections.Specialized;
+using System.Net;
+using System.Text;
 
 namespace Lav_Vino_Project
 {
@@ -7,6 +9,32 @@ namespace Lav_Vino_Project
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+
+        }
+
+        protected void signup_Click(object sender, EventArgs e)
+        {
+            string url = "http://192.168.0.6/Sign_up.php";
+            using (WebClient client = new WebClient())
+            {
+                NameValueCollection signup_info = new NameValueCollection() {
+                {"username", username.Text},
+                { "password", password.Text},
+                { "employee_id", eid.Text}
+            };
+
+                String pagesource = Encoding.UTF8.GetString(client.UploadValues(url, signup_info));
+
+                if (String.Compare(pagesource,"success") == 1)
+                {
+                    
+                    Server.Transfer("Login.aspx", true);
+                }
+               
+            }
+
+
+
 
         }
     }
---
+++ PHP_files/Sign_up.php
@@ -0,0 +1,31 @@
+<?php
+    
+        $uname = $_POST["username"];
+        $pwd = $_POST["password"];
+	$eid = $_POST['employee_id'];
+		
+
+	$con = mysqli_connect("localhost","root","root");
+        if (!$con)
+        {
+        die('Could not connect: ' . mysqli_error());
+        }
+        mysqli_select_db($con, "employee");
+	$result = mysqli_query($con,"INSERT INTO signup VALUES ('".$uname."','".$pwd."','".$eid."')");
+       
+       	if($result)
+        {
+		echo "success";
+        }
+        else
+       	{
+         	echo "failure";
+        }
+        mysqli_close($con);
+?>
+
+
+
+
+
+
